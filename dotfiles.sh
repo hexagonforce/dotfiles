65a7a05011b419a79cce002ed3634fbb1ee6742e
Hyeong Seon Yoo (유형선) <sylvesteryoo@naver.com>
2023-01-30 17:00:06 +0800

    fedora reset due to unfortunate circumstances

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,12 +4,13 @@ CONFIG_DIR="$HOME/.config"
 
 # Files inside the normal xdg .config directory
 
-xdg_dotfiles=("kitty/kitty.conf" "mpv/input.conf" "ranger/rc.conf" "ranger/rifle.conf")
-xdg_dotfiles+=("qutebrowser/config.py" "qutebrowser/quickmarks")
+xdg_dotfiles=("mpv/input.conf" "ranger/rc.conf" "ranger/rifle.conf")
 xdg_dotfiles+=("sway/config" "wofi/style.css" "zathura/zathurarc")
+xdg_dotfiles+=("foot/foot.ini")
 
 for f ($xdg_dotfiles){
 rm "$CONFIG_DIR/$f"
+mkdir -p "$CONFIG_DIR/$(dirname $f)"
 ln -s "$PWD/$f" "$CONFIG_DIR/$f"
 }
 
--- qutebrowser/config.py
+++
@@ -1,9 +0,0 @@
-## Configuration file for qutebrowser
-
-# Custom key bindings
-config.bind('<Ctrl+Shift+y>', 'hint links spawn --detach mpv --force-window yes {hint-url}' )
-config.bind('<Ctrl+Shift+p>', 'hint links spawn --detach mpv --no-video --force-window yes {hint-url}')
-config.bind('<Ctrl+i>', 'hint images download')
-config.bind('J', 'tab-prev')
-config.bind('K', 'tab-next')
-config.load_autoconfig(False)
