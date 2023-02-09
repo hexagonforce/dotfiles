0800234601b7524a2502f4316d4af802e8950417
Hyeong Seon Yoo (유형선) <sylvesteryoo@naver.com>
2023-02-09 13:52:10 +0800

    waybar configuration

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,12 +6,15 @@ CONFIG_DIR="$HOME/.config"
 
 xdg_dotfiles=("mpv/input.conf" "ranger/rc.conf" "ranger/rifle.conf")
 xdg_dotfiles+=("sway/config" "wofi/style.css" "zathura/zathurarc")
+xdg_dotfiles+=("waybar/config" "waybar/style.css")
 xdg_dotfiles+=("foot/foot.ini")
 
-for f ($xdg_dotfiles){
-rm "$CONFIG_DIR/$f"
-mkdir -p "$CONFIG_DIR/$(dirname $f)"
-ln -s "$PWD/$f" "$CONFIG_DIR/$f"
+for file ($xdg_dotfiles){
+    if [[ -f "$file" ]]; then
+        rm "$CONFIG_DIR/$file"
+    fi
+    mkdir -p "$CONFIG_DIR/$(dirname $file)"
+    ln -s "$PWD/$file" "$CONFIG_DIR/$file"
 }
 
 # Individual configuration files
