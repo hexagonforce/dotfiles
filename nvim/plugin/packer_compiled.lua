fce7b7261a0f114838f8c8b696ed9244da482eec
Hyeong Seon Yoo (유형선) <sylvesteryoo@naver.com>
2023-10-08 12:08:01 +0900

    qutebrowser configuration change

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/syoo/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/syoo/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/syoo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/syoo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/syoo/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/syoo/.cache/nvim/packer_hererocks/2.1.1694285958/share/lua/5.1/?.lua;/home/syoo/.cache/nvim/packer_hererocks/2.1.1694285958/share/lua/5.1/?/init.lua;/home/syoo/.cache/nvim/packer_hererocks/2.1.1694285958/lib/luarocks/rocks-5.1/?.lua;/home/syoo/.cache/nvim/packer_hererocks/2.1.1694285958/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/syoo/.cache/nvim/packer_hererocks/2.1.1694285958/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -74,49 +74,49 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
-  ["dracula.nvim"] = {
-    loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/dracula.nvim",
-    url = "https://github.com/Mofiqul/dracula.nvim"
-  },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
+  },
+  ["onedark.nvim"] = {
+    loaded = true,
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/onedark.nvim",
+    url = "https://github.com/navarasu/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   vimwiki = {
     loaded = true,
-    path = "/home/syoo/:/var/lib/flatpak/exports/:/var/lib/flatpak/exports/share:/var/lib/flatpak/exports/share/nvim/site/pack/packer/start/vimwiki",
+    path = "/home/syoo/.local/share/nvim/site/pack/packer/start/vimwiki",
     url = "https://github.com/vimwiki/vimwiki"
   }
 }
--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,10 +6,16 @@ config.bind('H', 'tab-prev')
 config.bind('L', 'tab-next')
 config.bind(';v', 'hint links spawn mpv "{hint-url}"')
 config.bind(';g', 'hint images download')
+config.bind('j', 'scroll-px 0 96')
+config.bind('k', 'scroll-px 0 -96')
 config.set('hints.chars', 'arstdhneiocm')
 
 config.set('downloads.location.prompt', False)
-tab_bg_color = '#6272A4'
-config.set('colors.tabs.selected.even.bg', tab_bg_color)
-config.set('colors.tabs.selected.odd.bg', tab_bg_color)
+config.source('qutebrowser-themes/themes/onedark.py')
 
+tab_bg_color = '#61AFEF'
+tab_text_color = '#282C34'
+config.set('colors.tabs.selected.even.bg', tab_bg_color)
+config.set('colors.tabs.selected.even.fg', tab_text_color)
+config.set('colors.tabs.selected.odd.bg', tab_bg_color)
+config.set('colors.tabs.selected.odd.fg', tab_text_color)
